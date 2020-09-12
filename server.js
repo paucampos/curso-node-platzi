b774107754e385331b6cf2fb19fca097b0f95b29
PauCampos <pau.campos89@gmail.com>
2020-09-12 17:51:15 -0300

    ejemplos de res con body y query

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
 
 router.get('/message', function(req, res) {
+    res.send('Lista de mensajes');
+});
+router.post('/message', function(req, res) {
     res.send('Mensaje Añadido');
+});
+router.delete('/message', function(req, res) {
+    console.log(req.query);
+    console.log(req.body);
+    res.send(`Mensaje "${req.body.text}" Eliminado correctamente`);
 });
 
 app.listen(3000);
