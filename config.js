60d10ef1ed2b4d111800bbdffc1455ea208dba99
PauCampos <pau.campos89@gmail.com>
2020-09-14 22:21:18 -0300

    variables globales + cors

--- config.js
+++ config.js
@@ -1,5 +1,12 @@
-module.exports = {
-    DB_HOST: 'user_pau',
-    DB_PORT: '6OdsxovZCj2a5QEY',
-    DB_NAME: 'telegrom'
-}
+const config = {
+    DB_HOST: process.env.DB_HOST || 'user_pau',
+    DB_PORT: process.env.DB_PORT || '6OdsxovZCj2a5QEY',
+    DB_NAME: process.env.DB_NAME || 'telegrom',
+    dbUrl: process.env.DB_URL || 'mongodb://user:user1234@ds255107.mlab.com:55107/telegrom',
+    port: process.env.PORT || 3000,
+    host: process.env.HOST || 'http://localhost',
+    publicRoute: process.env.PUBLIC_ROUTE || '/app',
+    filesRoute: process.env.FILES_ROUTE || 'files'
+};
+
+module.exports = config;
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const db = require('./db');
 const router = require('./nertwork/routes');
-const { DB_HOST, DB_PORT, DB_NAME } = require('./config');
-const connectionUrl = `mongodb+srv://${DB_HOST}:${DB_PORT}@cluster0-xvqsc.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+const config = require('./config');
+const app = express();
+const connectionUrl = `mongodb+srv://${config.DB_HOST}:${config.DB_PORT}@cluster0-xvqsc.mongodb.net/${config.DB_NAME}?retryWrites=true&w=majority`;
+const cors = require('cors');
 
-var app = express();
+app.use(cors());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 router(app);
@@ -14,7 +16,8 @@ router(app);
 db(connectionUrl);
 
 // Servir archivos estáticos
-app.use('/app', express.static('public'));
+app.use(config.publicRoute, express.static('public'));
 
-app.listen(3000);
-console.log('La aplicación está escuchando en http://localhost:3000');
+app.listen(config.port, function() {
+    console.log('La aplicación está escuchando en ' + config.host + ':' + config.port);
+});
